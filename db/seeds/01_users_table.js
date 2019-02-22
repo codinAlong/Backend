45d103bd64b96ab51538c185bcb38830f64acacf
Joey Gaitan <joeygaitan707@gmail.com>
2019-02-22 14:18:13 -0800

    successfully finished migrations now gonna start working on the auth and user routes

---
+++ db/seeds/01_users_table.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, fname: "sam", lname: 'summers', username:"pandasummers", password: "", img:"../../images/panda.jpeg"},
+        {id: 2, fname: "hank", lname: 'redford', username:"foxford", password: "", img:"../../images/fox.jpeg"},
+        {id: 3, fname: "fill", lname: 'crowns',username:"fillcrown", password: "", img:"../../images/crow.jpeg"}
+      ])
+      .then(()=>{
+        return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`)
+      })
+    });
+};
--- db/seeds/02_chatrooms_table.js
+++ db/seeds/02_chatrooms_table.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('chatrooms').insert([
-        {id: 1, links:"https://github.com/codinAlong",img:"otherstuff"},
-        {id: 2, links:"https://devhints.io/knex",img:"blah"},
-        {id: 3, links:"https://github.com/joeygaitan/moviecrud-backend/",img:"otherstuff"}
+        {id: 1, user_id: 1 , links:"https://github.com/codinAlong",img:"otherstuff"},
+        {id: 2, user_id: 2, links:"https://devhints.io/knex",img:"blah"},
+        {id: 3, user_id: 2, links:"https://github.com/joeygaitan/moviecrud-backend/",img:"otherstuff"}
       ])
       .then(()=>{
         return knex.raw(`SELECT setval('chatrooms_id_seq', (SELECT MAX(id) FROM chatrooms));`)
